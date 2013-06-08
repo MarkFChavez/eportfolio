e22f32a06d6fd309baefbfc581cc1c3396a8ebb9
Mark Chavez <los_populares@yahoo.com>
2013-06-08 17:40:46 +0800

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,5 @@
+module ApplicationHelper
+	def title(page_title)
+		content_for(:title) do page_title end
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eportfolio::Application.config.secret_token = '36c662f1881513253e351802e2dff348d4f8bf6dd26440fd10c2ccd5ffae4f4a4bb07cdb1167464ed7d0e1bfcda190600a9bc783f78d5ce99ff4d0fccfe41fb8'
